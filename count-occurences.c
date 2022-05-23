fca512b34eda0acf07b27a0ab1664e41e096e930
mariia-ivanski <mariia.ivanski@gmail.com>
2022-05-23 22:22:48 +0300

    update solution

--- count-occurences.c
+++ count-occurences.c
@@ -1,17 +1,24 @@
 
 /*count the frequency of each element of an array */
 
+
+
 int main(){
 int arr[] = {1, 2, 4, 5, 4, 1};
 int arr2[6];
 int length = 6; 
 int i,j, times;
+for (i=0; i < length; i++)
+{
+    arr2[i] = -1;
+}
+
     for (i=0; i < length; i++)
     {
     times = 1; 
 
             for (j=i+1; j < length; j++){
-             arr2[i] = -1;
+             
                 if(arr[i] == arr[j])
                 {
                 times++;
