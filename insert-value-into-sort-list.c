9321f5633cce08d40dd49cf37f7f20e9085a5d43
mariia-ivanski <mariia.ivanski@gmail.com>
2022-05-25 17:56:37 +0300

    add sol. to insert value into sorted list

---
+++ insert-value-into-sort-list.c
@@ -0,0 +1,42 @@
+int printArray (int array[], int size);
+
+int main(){
+    int arr[] = {2, 5, 7, 9, 11};
+    int size = 5;
+    int arr2[6];
+    int size2 = 6;
+
+    int i, j = 8;
+
+    for(i = 0; i < size; i++)
+    {
+        if (arr[i] <= j)
+        {
+            arr2[i] = arr [i];
+        } else {
+            arr2[i+1] = arr[i];
+        }
+    }
+
+    for(i = 0; i<size2; i++)
+    {
+        if (arr2[i] != arr[i])
+        {
+            arr2[i] = j;
+            break;
+        }
+    }
+
+    printArray(arr, size);
+    printArray(arr2, size2);
+
+}
+
+int printArray (int array[], int size){
+int i;
+printf("\n");
+for(i=0; i<size; i++)
+{
+    printf("%d ",array[i]);
+}
+}
