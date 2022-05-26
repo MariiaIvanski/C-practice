23d3c1a871dee9779829575d556e0e4edbc0b758
mariia-ivanski <mariia.ivanski@gmail.com>
2022-05-26 15:04:17 +0300

    add solution to reverse array using ptr

---
+++ reverse-array-w-pointer.c
@@ -0,0 +1,26 @@
+int reverseArray(int *ptr, int length);
+
+int main(){
+    int arr[]= {2, 3, 4, 5, 6};
+    int length =5;
+    int *ptr;
+    ptr = arr;
+
+reverseArray(ptr, length);
+
+}
+
+int reverseArray(int *ptr, int length){
+    int i; 
+    int temp;
+
+    for (i=0; i <= length/2; i++){
+        temp = ptr[i];
+        ptr[i] = ptr[length-i-1];
+        ptr[length-i-1] = temp;
+    }
+
+     for(i=0; i<length; i++){
+    printf("element %d : %d \n", i, ptr[i]);    
+    }
+}
