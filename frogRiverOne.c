dff49cb8bf68fd13dcda4bc6444245ce3a9317af
mariia-ivanski <mariia.ivanski@gmail.com>
2022-05-31 22:56:22 +0300

    update solution

--- frogRiverOne.c
+++ frogRiverOne.c
@@ -1,22 +1,35 @@
-int solution(int X, int A[], int N);
 
-int main(){
-    int arr[] = {1, 3, 1, 4, 2, 3, 5, 4};
-    int length = 7;
-    int max = 5;
 
-    int jump = solution(max, arr, length);
-    printf("time to jump: %d", jump);
-}
 
 int solution(int X, int A[], int N){
-    int i;
-    int temp = 0;
-    for(i=0; i<N; i++){
-        if(A[i] == X){
-            temp = i;
-            break;
+    int i, j = 1;
+    
+
+   int *arr2 =  calloc(sizeof(int), X + 1);
+
+    for(i=0; i < N; ++i )
+    {
+        if(A[i] <= X)
+        {
+           arr2[A[i]] = 1;
+             
         }
-        return temp;
+        for(j = 1; j <= X; ++j )
+        {
+            if(arr2[j] != 1)
+            {
+              break;
+            }
+        } 
+        if(j == X + 1)
+        {
+            return i;
+        } 
+        
+    
     }
+    
+   return -1;
+
+
 }
