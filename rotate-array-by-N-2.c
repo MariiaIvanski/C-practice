c3399a16ee5a819f8d6dde4440b246b07d683ce7
mariia-ivanski <mariia.ivanski@gmail.com>
2022-06-11 19:46:51 +0300

    update include

--- rotate-array-by-N-2.c
+++ rotate-array-by-N-2.c
@@ -1,10 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 int rotateArr(int *array1, int length, int N);
 void printArr(int *array1, int length);
 
 int main(){
     int arr1[]= {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
     int length = 10;
-    int N = 10; 
+    int N = 5; 
 
 rotateArr(arr1, length, N);
 printArr(arr1, length);
